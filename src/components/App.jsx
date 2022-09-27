05cff12772f3f7cbdf734c565290d27e8ed76927
jenna-graham <jclayto2@gmail.com>
2022-09-27 10:59:07 -0700

    initial setup complete

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div>App</div>
+  );
+}
