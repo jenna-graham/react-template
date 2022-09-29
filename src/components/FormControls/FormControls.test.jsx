7a469c7bb0c4904600f67ac02ba71c98db5423fa
jenna-graham <jclayto2@gmail.com>
2022-09-29 12:39:31 -0700

    input test passing

--- src/components/FormControls/FormControls.test.jsx
+++ src/components/FormControls/FormControls.test.jsx
@@ -1,9 +1,18 @@
 import { render, screen } from '@testing-library/react';
-
-
+import { InputControl } from './FormControls.jsx';
 
 test('Input Control', async () => {
-    render(
+  render(
+    <InputControl
+      label="Name"
+      name="name"
+      required
+      placeholder="your name"
+    />
+  );
 
-    )
-})
+  const inputControl = screen.getByLabelText('Name');
+  expect(inputControl.name).toBe('name');
+  expect(inputControl.placeholder).toBe('your name');
+  expect(inputControl.required).toBe(true);
+});
