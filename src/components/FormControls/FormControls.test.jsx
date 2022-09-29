c618cb2ad208eaa30c5ede7dcd2d412e5847f021
jenna-graham <jclayto2@gmail.com>
2022-09-29 12:45:59 -0700

    text area test passing

--- src/components/FormControls/FormControls.test.jsx
+++ src/components/FormControls/FormControls.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { InputControl } from './FormControls.jsx';
+import { InputControl, TextAreaControl } from './FormControls.jsx';
 
 test('Input Control', async () => {
   render(
@@ -16,3 +16,18 @@ test('Input Control', async () => {
   expect(inputControl.placeholder).toBe('your name');
   expect(inputControl.required).toBe(true);
 });
+
+test('TextArea Control', async () => {
+  render(
+    <TextAreaControl
+      label="Tell us more"
+      name="bio"
+      required
+      placeholder="tell us about yourself"
+    />
+  );
+  const textAreaControl = screen.getByLabelText('Tell us more');
+  expect(textAreaControl.name).toBe('bio');
+  expect(textAreaControl.required).toBe(true);
+  expect(textAreaControl.placeholder).toBe('tell us about yourself');
+})
